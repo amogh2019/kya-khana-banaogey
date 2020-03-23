cdf1d002c632692dc251c8f8d86aeb2fd5cccf2c
amogh <amogh.sharma@gds.ey.com>
2020-03-23 20:21:40 +0530

    Updates

---
+++ precache-manifest.6cc75c20fdf43cf64ade35a7f0cfdae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e65bd3ad3675ed7da5baf9ce2cdaaf2",
+    "url": "/kya-khana-banaogey/index.html"
+  },
+  {
+    "revision": "ea49f249ff2307492b80",
+    "url": "/kya-khana-banaogey/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "ea49f249ff2307492b80",
+    "url": "/kya-khana-banaogey/static/js/2.b41660b0.chunk.js"
+  },
+  {
+    "revision": "c6b75e2cb5efecbdf77c",
+    "url": "/kya-khana-banaogey/static/js/main.ff2f196e.chunk.js"
+  },
+  {
+    "revision": "1e09fbf35ccba5bacff0",
+    "url": "/kya-khana-banaogey/static/js/runtime~main.913e4146.js"
+  }
+]);
